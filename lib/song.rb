a3b36d3d0fd9b66d7b6e2dae82c972762b3c13f9
Andres Carvajal <jc1993box@hotmail.com>
2020-05-04 04:16:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,15 @@ class Song
 
   attr_accessor :name, :artist, :genre
     @@count = 0
+    @@artists = {}
   
 def initialize(song_name, artist, genre)
   @name = song_name
   @artist = artist
   @genre = genre 
   @@count += 1
+  @@artists
 end 
 
-def self.count
-  @@count
-end
+
 end
