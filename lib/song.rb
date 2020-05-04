fb34c5429e8df74cf48a8f0e4e1c72f26e25c684
Andres Carvajal <jc1993box@hotmail.com>
2020-05-04 04:17:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,14 @@ class Song
 
   attr_accessor :name, :artist, :genre
     @@count = 0
-    @@artists = {}
+    @@artists = []
   
 def initialize(song_name, artist, genre)
   @name = song_name
   @artist = artist
   @genre = genre 
   @@count += 1
-  @@artists
+  @@artists << artist
 end 
 
 
